4fa68c9b1fdd2c97fd13f4778d6bd58a23dda161
Viktor Varland <viktor@vardevs.se>
2016-04-01 13:53:38 +0200

    make port configurable via process.env

--- server.js
+++ server.js
@@ -52,6 +52,7 @@ app.get('/place/:location', function (req, res) {
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log('Example app listening on port %d!', port);
 });
